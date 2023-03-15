fb99a048daa8e167b1446001d552c5a1ff86c0ad
Binhkio <quan.va205115@gmail.com>
2023-03-15 14:36:30 +0700

    pc transform

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 discord.js: ^14.7.1
 */
 
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const { default: axios } = require('axios');
 const { 
     REST, 
     Routes, 
@@ -11,7 +12,12 @@ const {
     SlashCommandBuilder,
     ChannelType
 } = require('discord.js');
+const { createReadStream } = require('fs');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const googleTTS = require('google-tts-api')
+const fs = require('fs')
+const pathToFfmpeg = require('ffmpeg-static')
 
 const tokenArr = [
     "MTA4MzczMjg1NTUxMjg5OTYzNQ",
@@ -70,17 +76,42 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
+var currentConnection = null
+
 client.on('interactionCreate', async interaction => {
     try {
         if (!interaction.isChatInputCommand()) return;
         
+        // VOICE SPEAK
         if (interaction.commandName === 'say') {
             const voiceMessage = interaction.options.getString('content')
-            console.log(voiceMessage)
+            const voiceConnection = getVoiceConnection(interaction.guildId)
+
+            const player = createAudioPlayer()
+            voiceConnection.subscribe(player)
+            
+            const voiceURL = googleTTS.getAudioUrl(voiceMessage, {
+                lang: 'vi',
+                slow: false,
+                host: 'https://translate.google.com',
+                splitPunct: ',.?',
+            })
+            const { data } = await axios.get(voiceURL, {
+                responseType: 'arraybuffer',
+                headers: {
+                    "Content-Type": 'audio/mpeg'
+                }
+            })
+            const writer = fs.createWriteStream('./audio.mp3')
+            writer.write(data)
+
+            const resource = createAudioResource(createReadStream('./audio.mp3'))
+            player.play(resource)
 
             await interaction.reply(`${voiceMessage}`);
         }
 
+        // JOIN CHANNEL
         if (interaction.commandName === 'join') {
             const voiceChannel = interaction.options.getChannel('channel')
             const voiceConnection = joinVoiceChannel({
@@ -88,8 +119,16 @@ client.on('interactionCreate', async interaction => {
                 guildId: interaction.guildId,
                 adapterCreator: interaction.guild.voiceAdapterCreator,
             })
-
+            currentConnection = voiceConnection
             await interaction.reply(`Join channel **${voiceChannel.name}**`)
+        }
+
+        // DISCONNECT
+        if (interaction.commandName === 'disconnect') {
+            const voiceConnection = getVoiceConnection(interaction.guildId)
+            voiceConnection.disconnect()
+
+            await interaction.reply(`Bot has disconnected`)
         }
     } catch (error) {
         console.log(error);
